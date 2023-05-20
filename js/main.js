add04d9ee50b8c38fb218b4cabe66c5dea5c1d1c
Siraj Ahmed <mdsiraj101@gmail.com>
2023-05-20 18:08:38 +0530

    Done

--- js/main.js
+++ js/main.js
@@ -14,20 +14,44 @@ const sliderMovieData = async () => {
 sliderMovieData();
 
 function getSliderMovieData(data) {
-  return data.forEach((element) => {
-    const slider = document.querySelector(".carousel-inner");
-    slider.innerHTML += `
-      
-        <div class="carousel-item ">
-            <img src="${movieImg}${element.poster_path}" class="d-block w-100 img-fluid" alt="..." />
-            <div class="carousel-caption">
-                <h2>${element.original_title}</h2>
-                <p>
-                ${element.overview}
-                </p>
-            </div>
-        </div>`;
+  const carouselInner = document.querySelector(".carousel-inner");
+  const carouselBtn = document.querySelector(".carousel-indicators");
+  data.forEach((element, i) => {
+    carouselInner.innerHTML += `
+      <div class="carousel-item">
+        <img src="${movieImg}${
+      element.backdrop_path
+    }" class="d-block w-100 " alt="..." />
+        <div class="overlay"></div>
+
+        <div class="carousel-caption d-none d-md-block">
+          <h2>${element.original_title}</h2>
+          <p > Year:
+          ${element.release_date.slice(0, 4)}  &nbsp &nbsp
+          <span>Rating: <i class="fas fa-star text-warning "></i>&nbsp ${
+            element.vote_average
+          }</span> </p> 
+          
+          <a href="#" class="btn primary-btn fs-3 text-center ">More Details  <i class="fas fa-plus-circle fs-1 "></i> </a>
+          
+        </div>
+      </div>`;
+
+    carouselBtn.innerHTML += ` <button
+      type="button"
+      data-bs-target="#carouselExampleCaptions"
+      data-bs-slide-to="${i}"
+      class="slide-btn"
+      aria-current="true"
+      aria-label="Slide ${i + 1}"
+    ></button>`;
   });
+
+  const carouselItem = document.querySelectorAll(".carousel-item")[0];
+  carouselItem.classList.add("active");
+
+  const slideBtn = document.querySelectorAll(".slide-btn")[0];
+  slideBtn.classList.add("active");
 }
 
 // Fetch Latest Movies  Data
@@ -44,30 +68,28 @@ function getLatestMovieData(data) {
     const latestMovies = document.querySelector("#latest-movies");
 
     latestMovies.innerHTML += `
-          <div class="col-lg-2 col-md-3 col-6 my-2 movie-list" >
+    <div class="col-lg-2 col-md-3 col-6 my-2 movie-list" >
               <div class="card" style="width: 18rem; height: 36rem;">
                   <img  src="${movieImg}${
       element.poster_path
     }" class="card-img-top img-fluid" alt="..." />
                   <div class="card-body">
-                      <h5 class="card-title">${element.original_title.slice(
-                        0,
-                        15
-                      )} ${element.original_title && "..."}</h5>
-                      <div class="d-flex justify-content-between my-4">
-                       <p class="card-text">${element.release_date.slice(
+                       <h5 class="card-title">${element.original_title.slice(
                          0,
-                         4
-                       )}</p> <p class="card-text"><i class="fas fa-star text-warning"></i> &nbsp${
-      element.vote_average
-    }</p>
-                       </div>
-                      
-                    
-                  </div>
-              </div>
-              </div>
-          </div>`;
+                         15
+                       )} ${element.original_title && "..."}</h5>
+                       <div class="d-flex justify-content-between my-4">
+                          <p class="card-text">${element.release_date.slice(
+                            0,
+                            4
+                          )}</p>
+                          <p class="card-text"><i class="fas fa-star text-warning"></i> &nbsp${
+                            element.vote_average
+                          }</p>
+                        </div>
+                    </div>                          
+               </div>
+        </div>`;
   });
 }
 // Fetch Upcoming Movies  Data
@@ -109,3 +131,7 @@ function getUpcomingMovieData(data) {
           </div>`;
   });
 }
+
+// --------------------
+//  ha ab bhai sun
+// mujhe na slider me or image add krni hai
